24500d772a065974cfbc3f9bfbf92a256f746691
bdomingu-ull <bdomingu@ull.edu.es>
2026-01-26 17:25:17 +0000

    Añadida plantilla de Latex en Zip

--- scripts/leer_datos_fuentes_oficiales.R
+++ scripts/leer_datos_fuentes_oficiales.R
@@ -69,10 +69,10 @@ leer_excel <- function(ruta_fichero, hoja) {
 # Modifica la ruta del fichero para adaptarla a la carpeta en la que tienes almacenados los datos
 # En la ruta, debemos usar barras del tipo '/' o no compilará el código
 ruta_excel_serpavi <- "C:/Users/marco/OneDrive/Escritorio/Marcos/Universidad/4º Carrera/TFG/2025-09-10_bd_SERPAVI_2011-2023.xlsx"
+#ruta_excel_serpavi <- "C:/Users/benco/Documents/tfg_viviendas_vacacionales/data/2025-09-10_bd_SERPAVI_2011-2023.xlsx"
 hoja_excel_serpavi <- "Municipios"
 datos_serpavi <- leer_excel(ruta_excel_serpavi, hoja_excel_serpavi)
 
-"C:/Users/benco/Documents/ULL/Curso 2025-2026 ULL/TFG/Ficheros/2025-09-10_bd_SERPAVI_2011-2023.xlsx"
 
 # ============================================================
 # Descarga del Censo de Viviendas 2021 (INE, API JSON)
